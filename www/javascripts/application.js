1604727b28d1b8118390bb2197d846af029cd880
Jeffrey Richardson <jeffreywebdevelopment@gmail.com>
2014-01-03 00:46:59 -0600

    first commit

---
+++ www/javascripts/application.js
@@ -0,0 +1,20 @@
+
+// steroids.view.navigationBar.show("Hello World");
+
+steroids.view.navigationBar.show("Pure Rent");
+
+function login() {
+  var webView = new steroids.views.WebView("renter-dashboard.html");
+  steroids.layers.push(webView);
+}
+
+function loginForm() {
+  login();
+  return false;
+}
+
+function settings() {
+  var webView = new steroids.views.WebView("renter-settings.html");
+  steroids.layers.push(webView);
+}
+
